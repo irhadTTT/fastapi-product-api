74ccb80fb51650289b2cd22a29119af24c0f13ec
Irhad <irhad.kun@gmail.com>
2026-07-29 21:11:31 +0200

    Dockerize FastAPI application with PostgreSQL service

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-DATABASE_URL="postgresql://fastapi:irhad@localhost/fastapi_products"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL
