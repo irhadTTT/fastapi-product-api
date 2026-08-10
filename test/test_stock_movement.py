1d6fab38eec9f825a60185cbf4d764e5e1c714c9
Irhad <irhad.kun@gmail.com>
2026-08-10 05:27:02 +0200

    added more integration tests from stock_movment.py

---
+++ test/test_stock_movement.py
@@ -0,0 +1,134 @@
+from enums.stock_movement_type import StockMovementType
+from models.product import Item
+from models.stock_movement import StockMovement
+
+
+def test_create_stock_movement_in(client, auth_headers, current_user, db_session):
+    product1 = Item(name="TestProduct1", price=100, stock_quantity=10)
+
+    db_session.add(product1)
+    db_session.commit()
+    db_session.refresh(product1)
+
+    response = client.post(
+        "/stock-movements",
+        json={
+            "product_id": product1.id,
+            "type": StockMovementType.IN,
+            "quantity": 20,
+            "note": "This is test",
+        },
+        headers=auth_headers,
+    )
+
+    assert response.status_code == 201
+    assert response.json()["product_id"] == product1.id
+    assert response.json()["user_id"] == current_user.id
+    assert response.json()["type"] == "IN"
+    assert response.json()["quantity"] == 20
+    assert response.json()["note"] == "This is test"
+
+    db_session.refresh(product1)
+    assert product1.stock_quantity == 30
+
+
+def test_create_stock_movement_out(
+    client,
+    auth_headers,
+    db_session,
+):
+    product = Item(name="TestProduct", price=100, stock_quantity=50)
+
+    db_session.add(product)
+    db_session.commit()
+    db_session.refresh(product)
+
+    response = client.post(
+        "/stock-movements",
+        json={
+            "product_id": product.id,
+            "type": StockMovementType.OUT,
+            "quantity": 20,
+            "note": "Stock outgoing",
+        },
+        headers=auth_headers,
+    )
+
+    assert response.status_code == 201
+
+    db_session.refresh(product)
+    assert product.stock_quantity == 30
+
+
+def test_create_stock_movement_out_insufficient_stock(
+    client,
+    auth_headers,
+    db_session,
+):
+    product = Item(name="TestProduct", price=100, stock_quantity=5)
+
+    db_session.add(product)
+    db_session.commit()
+    db_session.refresh(product)
+
+    response = client.post(
+        "/stock-movements",
+        json={
+            "product_id": product.id,
+            "type": "OUT",
+            "quantity": 10,
+            "note": "Too much stock",
+        },
+        headers=auth_headers,
+    )
+
+    assert response.status_code == 400
+    assert "Not enough products" in response.json()["detail"]
+
+    db_session.refresh(product)
+    assert product.stock_quantity == 5
+
+
+def test_create_stock_movement_invalid_quantity(
+    client,
+    auth_headers,
+    db_session,
+):
+    product = Item(name="TestProduct", price=100, stock_quantity=10)
+
+    db_session.add(product)
+    db_session.commit()
+    db_session.refresh(product)
+
+    response = client.post(
+        "/stock-movements",
+        json={
+            "product_id": product.id,
+            "type": "IN",
+            "quantity": 0,
+            "note": "Invalid quantity",
+        },
+        headers=auth_headers,
+    )
+
+    assert response.status_code == 400
+    assert "Quantity must be greater than zero" in response.json()["detail"]
+
+
+def test_create_stock_movement_product_not_found(
+    client,
+    auth_headers,
+):
+    response = client.post(
+        "/stock-movements",
+        json={
+            "product_id": 999999,
+            "type": "IN",
+            "quantity": 10,
+            "note": "Missing product",
+        },
+        headers=auth_headers,
+    )
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Product not found"
